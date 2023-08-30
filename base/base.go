84e7da0b84541679d7ea97646372acc2b84cc91b
anyaguuu <ag156@rice.edu>
2023-08-30 16:02:41 -0500

    finished base complete

--- base/base.go
+++ base/base.go
@@ -1,1 +1,23 @@
 package base
+
+import "strings"
+
+type Base struct {
+	baseMap map[string]int
+}
+
+func New(inputMap map[string]int) Base { // returns pointer to base
+	return Base{baseMap: inputMap} // maps words to frequencies
+}
+
+// implements the interface!
+// return up to the first 10 words alphabetically that have the given base
+func (b Base) Complete(base string) []string {
+	ret := make([]string, 0, 10) // so empty slice of strings, cap = 10
+	for word := range b.baseMap {
+		if strings.Index(word, base) == 0 {
+			ret = append(ret, word)
+		}
+	}
+	return ret
+}
