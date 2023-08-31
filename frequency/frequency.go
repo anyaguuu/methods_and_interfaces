fb7955eba4ee31c95f5c9dcb34abc8990b4f53c2
anyaguuu <ag156@rice.edu>
2023-08-30 19:24:11 -0500

    my last revisions

--- frequency/frequency.go
+++ frequency/frequency.go
@@ -1,1 +1,26 @@
 package frequency
+
+import "strings"
+
+type Frequency struct {
+	freqMap map[string]int
+}
+
+func New(inputMap map[string]int) Frequency {
+	return Frequency{freqMap: inputMap}
+}
+
+// implement interface
+// return up to the first ten words in decreasing
+// frequency order that have the given base
+
+func (f Frequency) Complete(base string) []string {
+	newMap := make(map[string]int, len(f.freqMap))
+	for word, freq := range f.freqMap {
+		if strings.Index(word, base) == 0 { // check if has base first
+			newMap[word] = freq
+		}
+	}
+
+	// now sort new map in decreasing order of frequency
+}
